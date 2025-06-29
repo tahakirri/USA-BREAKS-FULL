c603d4f35d2f4b48d9fd765c31e5bb45c85918de
tahakirri <taha.kirri@gmail.com>
2025-06-29 23:02:03 +0100

    Update USA FORM.py

--- USA FORM.py
+++ USA FORM.py
@@ -4,7 +4,7 @@ st.markdown(
     """
     <div style='text-align: center; font-size: 60px; color: red; margin-top: 100px;'>
         <strong>Not Available</strong><br>
-        <span style='font-size: 80px; color: gray;'>Waiting For Management Approval/Payment </span>
+        <span style='font-size: 80px; color: gray;'>Waiting For Management Approval/Payment </span><br>
          <span style='font-size: 80px; color: gray;'>En Espera de Aprobación de la Gerencia / Pago </span>
     </div>
     """,
