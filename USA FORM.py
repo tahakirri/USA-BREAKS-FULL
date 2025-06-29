26c657ef57637c976f9b61efcbeb04b9c45c053b
tahakirri <taha.kirri@gmail.com>
2025-06-29 22:55:37 +0100

    Update USA FORM.py

--- USA FORM.py
+++ USA FORM.py
@@ -1,5 +1,11 @@
 import streamlit as st
 
-st.markdown"( managment approval)"
-   
+st.markdown(
+    """
+    <div style='text-align: center; font-size: 60px; color: red; margin-top: 100px;'>
+        <strong>Not Available</strong><br>
+        <span style='font-size: 80px; color: gray;'>Waiting For Management Approval/Payment </span>
+    </div>
+    """,
+    unsafe_allow_html=True
 )
