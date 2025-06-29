2143120af7747b776b504e3654d9ccae7a461e93
tahakirri <taha.kirri@gmail.com>
2025-06-29 22:51:34 +0100

    Update USA FORM.py

--- USA FORM.py
+++ USA FORM.py
@@ -1,9 +1,5 @@
 import streamlit as st
 
-# Set the title of the page (optional)
-st.title("Status Update")
-
-# Display the message
 st.markdown(
     """
     <div style='text-align: center; font-size: 24px; color: red; margin-top: 50px;'>
